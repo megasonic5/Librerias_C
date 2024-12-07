f2ce8fbaab25b5c636164e0de29df0bd13148ec6
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2024-12-07 12:16:43 +0100

    rubiks.h

---
+++ src/Creados/rubiks.h
@@ -0,0 +1,17 @@
+#ifdef _RUBIKS_H
+#define _RUBIKS_H 1
+
+typedef struct {
+    int tamano;
+    int *lados[6];
+} T_cubo_rubik;
+
+T_cubo_rubik *crearCubo(int tamano);
+void eliminarCubo(T_cubo_rubik *cubo);
+void giraCubo(T_cubo_rubik *cubo,int cara,int direccion);
+void desordenarCubo(T_cubo_rubik *cubo);
+void ordenaCubo(T_cubo_rubik *cubo);
+void cuboOrdenado(T_cubo_rubik *cubo);
+void mostrarCubo(T_cubo_rubik *cubo);
+void guardarCubo(T_cubo_rubik *cubo);
+#endif
