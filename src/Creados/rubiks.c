97da4a4c7e5fb488f3cacff82abdfdeff4638f84
megasonic5 <168662773+megasonic5@users.noreply.github.com>
2024-12-08 09:29:03 +0100

    rubiks.c

--- src/Creados/rubiks.c
+++ src/Creados/rubiks.c
@@ -4,22 +4,27 @@
 #include <stdio.h>
 #include "rubiks.h"
 
-T_cubo_rubik crearCubo(int tamano) {
+T_cubo_rubik crearCubo(int tamano_x,int tamano_y) {
+    int tamano = tamano_x*tamano_y;
     T_cubo_rubik *cubo = malloc(sizeof(T_cubo_rubik));
+    cubo->nombre=scanf("%s.txt",cubo->nombre);
     if(cubo==NULL) {
         printf("No hay memoria para el cubo");
-        exit(1);
+        eliminarCubo(cubo);
+        return;
     }
-    int *P=malloc(sizeof(int)*6*tamano*tamano);
-    if(P==NULL) {
+    int *ix=malloc(sizeof(int)*6*tamano);
+    if(ix==NULL) {
         printf("no hay memoria para el cubo");
-        exit(1);
+        eliminarCubo(ix);
     }
     for(int i=0;i<6;i++) {
-        cubo->lados[i]=P+i*tamano*tamano;
-        for(int ilado=0;ilado<tamano*tamano;ilado++) {
+        cubo->lados[i]=P+i*tamano;
+        for(int ilado=0;ilado<tamano;ilado++) {
             cubo->lados[i][ilado]=i;
         }
     }
-    return cubo;
+    fopen(cubo->nombre,"w");
+    fprintf(cubo->nombre,cubo);
+    fclose(cubo->nombre);
 }
