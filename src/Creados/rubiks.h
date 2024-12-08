c529ad5111d135ad16bfdbfedde73adcea5da510
albertodiez1980LaRioja <albertodiez1980@gmail.com>
2024-12-08 17:23:15 +0100

    corrección: corregir código

--- src/Creados/rubiks.h
+++ src/Creados/rubiks.h
@@ -1,18 +1,19 @@
-#ifdef _RUBIKS_H
-#define _RUBIKS_H 1
+#ifndef _RUBIKS_H
+    #define _RUBIKS_H 1
 
-typedef struct {
-    int tamano;
-    int *lados[6];
-    char *nombre;
-} T_cubo_rubik;
+    typedef struct {
+        int tamano;
+        int *lados[6];
+        char *nombre;
+    } T_cubo_rubik;
 
-T_cubo_rubik *crearCubo(int tamano_x,int tamano_y);
-void eliminarCubo(T_cubo_rubik *cubo);
-void giraCubo(T_cubo_rubik *cubo,int cara,int direccion);
-void desordenarCubo(T_cubo_rubik *cubo);
-void ordenaCubo(T_cubo_rubik *cubo);
-void cuboOrdenado(T_cubo_rubik *cubo);
-void mostrarCubo(T_cubo_rubik *cubo);
-void guardarCubo(T_cubo_rubik *cubo);
+    T_cubo_rubik *crearCubo(int tamano_x,int tamano_y);
+    void eliminarCubo(T_cubo_rubik *cubo);
+    void giraCubo(T_cubo_rubik *cubo,int cara,int direccion);
+    void desordenarCubo(T_cubo_rubik *cubo);
+    void ordenaCubo(T_cubo_rubik *cubo);
+    void cuboOrdenado(T_cubo_rubik *cubo);
+    void mostrarCubo(T_cubo_rubik *cubo);
+    void guardarCubo(T_cubo_rubik *cubo);
+
 #endif
